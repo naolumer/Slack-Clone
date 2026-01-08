fd11fc71b12f98fa5b6e1e95467750ef3fdc8a58
naolumer <naolumer3@gmail.com>
2026-01-08 10:21:58 +0300

    added convex auth package

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
+const Page = () => {
+    return (
+        <div>
+            Test page
+        </div>
+    )
+}
+
+export default Page
